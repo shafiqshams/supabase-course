42ab6bf20ee26d9bcd01ca3ff7bfea332462154c
Shafiq Shams <shafiqshamsessani@gmail.com>
2026-03-08 22:38:45 +0100

    add handleSubmit and insert new task to supabase db

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,51 @@
+import { useState } from "react";
 import "./App.css";
+import supabase from "./lib/supabase";
+
+const initialState = {
+  title: "",
+  desc: "",
+};
 
 function App() {
+  const [newTask, setNewTask] = useState(initialState);
+  const handleSubmit = async (e: SubmitEvent) => {
+    e.preventDefault();
+    const { error } = await supabase.from("tasks").insert(newTask).single();
+
+    if (error) {
+      console.error("Error adding task: ", error.message);
+      return;
+    }
+
+    setNewTask(initialState);
+  };
   return (
     <div style={{ maxWidth: "600px", margin: "0 auto", padding: "1rem" }}>
       <h2>Task Manager CRUD</h2>
 
       {/* Form to add a new task */}
-      <form style={{ marginBottom: "1rem" }}>
+      <form onSubmit={handleSubmit} style={{ marginBottom: "1rem" }}>
         <input
           type="text"
           placeholder="Task Title"
           style={{ width: "100%", marginBottom: "0.5rem", padding: "0.5rem" }}
+          onChange={(e) => {
+            setNewTask((prev) => ({
+              ...prev,
+              title: e.target.value,
+            }));
+          }}
         />
         <textarea
           placeholder="Task Description"
           style={{ width: "100%", marginBottom: "0.5rem", padding: "0.5rem" }}
+          onChange={(e) => {
+            setNewTask((prev) => ({
+              ...prev,
+              desc: e.target.value,
+            }));
+          }}
         />
         <button type="submit" style={{ padding: "0.5rem 1rem" }}>
           Add Task
