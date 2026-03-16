18290964c67fffc5de20be5b3b9acfdf9d8d40a3
Shafiq Shams <shafiqshamsessani@gmail.com>
2026-03-16 23:56:46 +0100

    add fetchSession from supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import { type Session } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+import supabase from "./lib/supabase";
 import { Auth } from "./pages/Auth";
 import TaskManager from "./pages/TaskManager";
 
 function App() {
   const [session, setSession] = useState<Session | null>(null);
+  useEffect(() => {
+    const fetchSession = async () => {
+      const {
+        data: { session: currentSession },
+      } = await supabase.auth.getSession();
+      console.log(currentSession);
+      setSession(currentSession);
+    };
+
+    fetchSession();
+  }, []);
   return (
     <>
       <TaskManager />
