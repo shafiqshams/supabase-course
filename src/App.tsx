8b0c118760a7ae87c3d5e237e913ccc013309a0f
Shafiq Shams <shafiqshamsessani@gmail.com>
2026-03-16 23:55:59 +0100

    add session state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { type Session } from "@supabase/supabase-js";
 import { Auth } from "./pages/Auth";
 import TaskManager from "./pages/TaskManager";
 
 function App() {
+  const [session, setSession] = useState<Session | null>(null);
   return (
     <>
       <TaskManager />
